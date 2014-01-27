47832cfd384aeb79fa655353047e49959b907a87
Muhammad-Sharif Moustafa <sharif@Muhammad-Sharifs-MacBook-Pro.local>
2014-01-27 10:00:47 -0500

    Initial Commit

---
+++ slider/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  slider
+//
+//  Created by Muhammad-Sharif Moustafa on 1/27/14.
+//  Copyright (c) 2014 Muhammad-Sharif Moustafa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ slider/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  slider
+//
+//  Created by Muhammad-Sharif Moustafa on 1/27/14.
+//  Copyright (c) 2014 Muhammad-Sharif Moustafa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
